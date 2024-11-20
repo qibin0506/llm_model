4a1b860b12d97211211204b36f1414ff667265bd
亓斌 <qibin0506@users.noreply.github.com>
2024-11-20 17:31:51 +0800

    Add files via upload

---
+++ test_llama.py
@@ -0,0 +1,27 @@
+import torch
+from llama import LlamaModel
+from llama import LlamaConfig
+from transformers import BertTokenizerFast
+
+
+def test_llama_model():
+    device = "cuda" if torch.cuda.is_available() else 'cpu'
+    tokenizer = BertTokenizerFast('./vocab.txt')
+
+    text = "hello world hello qibin"
+    input_ids = tokenizer.encode(text, add_special_tokens=False)
+    input_ids = torch.tensor(input_ids).long().unsqueeze(0).to(device)
+
+    llama = LlamaModel(config=LlamaConfig(vocab_size=tokenizer.vocab_size))
+    output = llama(input_ids)
+    print(output.shape)
+
+
+if __name__ == '__main__':
+    test_llama_model()
+
+
+
+
+
+
