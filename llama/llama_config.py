9ce76ff0630b08b4a12079270f88545e5bfb57bd
qibin <qibin0506@users.noreply.github.com>
2024-12-31 21:59:06 +0800

    Update llama_config.py

--- llama/llama_config.py
+++ llama/llama_config.py
@@ -37,7 +37,7 @@ class Config:
     hidden_size: int = 4096
     intermediate_size: int = 11008
     num_attention_heads: int = 32
-    num_key_value_heads: int = num_attention_heads
+    num_key_value_heads: int = 32
     max_position_embeddings: int = 2048
     rope_theta: float = 10000.0
     attention_dropout: float = 0.1
