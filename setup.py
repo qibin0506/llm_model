9cbb70c318066cf3c35d36b65f7a1938e8f281ca
qibin <qibin0506@users.noreply.github.com>
2025-06-10 11:10:00 +0000

    update version

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from setuptools import setup, find_packages
 
-# 1. python3 setup.py sdist bdist_wheel
-# 2. pip3 install dist/project_llm-model-0.1.tar.gz
 setup(name='project_llm_model',
-    version='0.2',
-    description='LLM model',
+    version='0.3',
+    description='LLM and VLM model',
     author='qibin',
     author_email='qibin0506@gmail.com',
-    packages=find_packages(),)
+    packages=find_packages()
+)
