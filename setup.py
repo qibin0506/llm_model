e7d51076bff05c8779377dd5244dcb089339af49
Bin Qi <qibin0506@users.noreply.github.com>
2026-01-05 17:58:47 +0800

    Bump version from 0.7.6 to 0.7.7

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='project_llm_model',
-    version='0.7.6',
+    version='0.7.7',
     description='LLM and VLM model',
     author='qibin',
     author_email='qibin0506@gmail.com',
